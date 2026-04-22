b8068a47aab74eb32a98ff7fa69b76263517710d
isaacferreira499-sudo <isaacferreira499@gmail.com>
2026-04-22 20:15:23 -0300

    Arrumar

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const TMBPage());
+  runApp(const MyApp());
 }
 
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Calculadora de Saúde',
+      theme: ThemeData(primarySwatch: Colors.cyan),
+      home: const TMBPage(),
+    );
+  }
+}
 
 class TMBPage extends StatefulWidget {
   const TMBPage({super.key});
